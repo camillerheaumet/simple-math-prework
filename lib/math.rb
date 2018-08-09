c735b1804df89514db83cda92ba02ab3e481eb77
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-09 12:44:15 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,20 +1,26 @@
 def addition(num1, num2)
   total = num1 + num2
+  total
 end
 
 def subtraction(num1, num2)
-    total = num1 - num2
+  total = num1 - num2
+  total
 end
 
 def division(num1, num2)
-    total = num1 / num2
+  total = num1 / num2
+  total
 end
 
 def multiplication(num1, num2)
-    total = num1 * num2
+  total = num1 * num2
+  total
 end
 
 def modulo(num1, num2)
+  total = num1 % num2
+  total
 end
 
 def square_root(num)
